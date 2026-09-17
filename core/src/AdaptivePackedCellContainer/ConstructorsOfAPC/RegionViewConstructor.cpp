be028f2acbc77ade84e9f90e3d80124bb5fbd418
nirab123456 <rifatulislam210@gmail.com>
2026-09-17 19:43:23 -0400

    MAKE SLAM LOCK FREE

--- core/src/AdaptivePackedCellContainer/ConstructorsOfAPC/RegionViewConstructor.cpp
+++ core/src/AdaptivePackedCellContainer/ConstructorsOfAPC/RegionViewConstructor.cpp
@@ -11,7 +11,6 @@ namespace BidirectionalInMemGraph
     ) noexcept
     {
         using SD = SchemaDefinition;
-        using ASG = APCStorageGeometry;
 
         std::span<SchemaDefinition::RegionSchemaRecord> region_row = APCCache_.FabricOwnerPtr_->MetrixViewRow_(
             static_cast<uint32_t>(APCCache_.APCSlotIdx_)
